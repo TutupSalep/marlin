5a7f5028092e49c8e30c7a48db075e6868a3fd76
Raka Settya <rakhasn2410@gmail.com>
2018-08-07 15:37:44 +0700

    add sc

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,38 +1,39 @@
 import React from 'react';
-import { StyleSheet, FlatList, Text } from 'react-native';
+import { StyleSheet, FlatList, Text, View, ListView } from 'react-native';
 
 export default class LinksScreen extends React.Component {
 
   _marlinCount(){
       let hasil = [];
       for(let i = 1; i<= 50; i++){
+          //hasil.push(i + "")
         if (this.hitungJumlah(hasil) === 5){
           break
         }
         if (this.hitungJumlah(hasil) === 2){
           if (this.kelipatanTiga(i) && this.kelipatanLima(i)){
-            hasil.push('Marlin Booking')
+            hasil.push(i + 'Marlin Booking')
               continue;
           }
           if (this.kelipatanTiga(i)) {
-            hasil.push('Booking')
+            hasil.push(i +'Booking')
               continue
           }
           if (this.kelipatanLima(i)) {
-            hasil.push('Marlin')
+            hasil.push(i +'Marlin')
               continue
           }
         } else {
             if (this.kelipatanTiga(i) && this.kelipatanLima(i)) {
-                hasil.push('Marlin Booking');
+                hasil.push(i +'Marlin Booking');
                 continue;
             }
             if (this.kelipatanTiga(i)) {
-                hasil.push('Marlin');
+                hasil.push(i + 'Marlin');
                 continue;
             }
             if (this.kelipatanLima(i)) {
-                hasil.push('Booking');
+                hasil.push(i + 'Booking');
                 continue;
             }
         }
@@ -63,10 +64,14 @@ export default class LinksScreen extends React.Component {
   render() {
     return (
         this._marlinCount().map((item) =>
+            <View styles={styles.container}>
             <FlatList
+                horizontal={true}
+                styles={styles.container}
                 data={item}
                 renderItem={({item}) => <Text>{item}</Text>}
             />
+            </View>
         )
     );
   }
